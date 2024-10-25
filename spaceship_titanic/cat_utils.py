cf93cdb930234fee9073f0ffad300a8f8dd7e238
Guillermo González <guillermogcordero@gmail.com>
2024-10-25 13:49:22 +0200

    Added a lot of things

--- spaceship_titanic/cat_utils.py
+++ spaceship_titanic/cat_utils.py
@@ -19,3 +19,51 @@ def home_destin(df):
         elif route == 'Unknown_PSO J318.5-22':
             df.loc[df['Route'] == route, 'Route'] = 'Mars_PSO J318.5-22'
     return df
+
+
+def vip(df):
+    # Fill VIP with the most common (False, by a huge difference)
+    mode_vip = df['VIP'].mode()[0]
+    df['VIP'].fillna(mode_vip, inplace=True)
+
+    # Convert to int
+    df['VIP'] = df['VIP'].astype(int)
+
+    return df
+
+
+def cryo_sleep(df):
+    df_mode_cryo = df.groupby('Route')['CryoSleep'].agg(lambda x: x.mode()[0])
+
+    # Fill NaN in Cryo with the most common bool based on the route
+    df['CryoSleep'] = df.apply(
+        lambda row: df_mode_cryo[row['Route']] if pd.isna(row['CryoSleep'])
+        else row['CryoSleep'],
+        axis=1)
+
+    # Convert to int
+    df['CryoSleep'] = df['CryoSleep'].astype(int)
+
+    return df
+
+
+def name(df):
+    df['Name'].fillna('Unknown', inplace=True)
+
+    # Create separated column for surnames only
+    df['Surname'] = df['Name'].apply(lambda x: str(x).split()[-1])
+
+    return df
+
+
+def cabin(df):
+    # Fill NaN based on Surname
+    df_surname_cabin = df.dropna(subset=['Cabin']).groupby('Surname')['Cabin'].first()
+    df['Cabin'] = df['Cabin'].fillna(df['Surname'].map(df_surname_cabin))
+    df['Cabin'] = df['Cabin'].fillna('Unknown')
+    df['Cabin'] = df['Cabin'].replace('Unknown', 'Unknown/Unknown/Unknown')
+
+    # Split Cabin into Deck, Number and Side
+    df[['Cabin_deck', 'Cabin_num', 'Cabin_side']] = df['Cabin'].str.split('/', expand=True)
+
+    return df
